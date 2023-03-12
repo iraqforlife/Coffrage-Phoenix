6820529c945de874c5ca899a5e8e9a18d0ec627e
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-03-12 13:35:28 -0400

    paper form

--- src/app/components/soumission/soumission.component.ts
+++ src/app/components/soumission/soumission.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-soumission',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./soumission.component.scss']
 })
 export class SoumissionComponent {
-
+  isFrench = true;
+  constructor(public translate: TranslateService){
+    this.isFrench = translate.currentLang == 'fr';
+  }
 }
