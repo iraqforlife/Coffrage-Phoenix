c3c1b8a61f33b254a1b4ff6e24475f90e28cbd05
Ahmad Al-Taher <ahmadaltahir@gmail.com>
2023-04-12 12:00:25 -0400

    blog

---
+++ src/app/components/blogs/comment-construire-une-fondation-en-beton/comment-construire-une-fondation-en-beton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-construire-une-fondation-en-beton',
+  templateUrl: './comment-construire-une-fondation-en-beton.component.html',
+  styleUrls: ['./comment-construire-une-fondation-en-beton.component.scss']
+})
+export class CommentConstruireUneFondationEnBetonComponent {
+
+}
