e70bbed8723df2fd7250bcbf78e2cc007631cf6e
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-04-14 05:27:55 -0400

    clean up realizations

--- src/app/components/realisations/realisations.component.ts
+++ src/app/components/realisations/realisations.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-realisations',
   templateUrl: './realisations.component.html',
+  styleUrls: ['./realisations.component.scss']
+
 })
 export class RealisationsComponent {
 
