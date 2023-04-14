ffab192561f653af63c005a1fae8a271350b3a08
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-04-14 07:02:43 -0400

    values refont

--- src/app/components/values/values.component.ts
+++ src/app/components/values/values.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./values.component.scss']
 })
 export class ValuesComponent {
+  values: any[] = [
+    {
+      h5:"Integrity",
+      p:"Integrity.p",
+      icon:"fa-user"
+    },
+    {
+      h5:"Quality",
+      p:"Quality.p",
+      icon:"fa-dumbbell"
+    },
+    {
+      h5:"Trust",
+      p:"Trust.p",
+      icon:"fa-handshake"
+    },
+    {
+      h5:"Innovation",
+      p:"Innovation.p",
+      icon:"fa-lightbulb"
+    },
 
+  ];
+  responsiveOptions = [
+    {
+        breakpoint: '1400px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '1220px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '900px',
+        numVisible: 1,
+        numScroll: 1
+    }
+  ];
 }
