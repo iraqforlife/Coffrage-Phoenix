82f3903627ba94f2fefa6ed0d98276c0fbdd9aaf
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-06-16 11:29:20 -0400

    home services links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,38 +12,56 @@ export class HomeComponent {
     {
       image :'coffrage-de-beton.jpg',
       name:'home.p1',
+      en:'services/formwork',
+      fr:'services/coffrage',
     },
     {
       image :'fondation.jpg',
       name:'home.p2',
+      en:'services/house-foundation',
+      fr:'services/fondation-maison',
     },
     {
       image :'agrandissement.jpg',
       name:'home.p4',
+      en:'services/house-extension',
+      fr:'services/agrandissement-maison',
     },
     {
       image :'balcon-de-beton.jpg',
       name:'concrete-balcony',
+      en:'services/concrete-balcony',
+      fr:'services/balcon-en-beton',
     },
     {
       image :'descente-de-sous-sol-en-beton_003.png',
       name:'home.p3',
+      en:'services/concrete-basement',
+      fr:'services/descente-sous-sol-beton',
     },
     {
       image :'escalier-de-beton.jpg',
       name:'concrete-staircase',
+      en:'services/concrete-step',
+      fr:'services/marche-de-beton',
     },
     {
       image :'dalle.jpeg',
       name:'concrete-dalle',
+      en:'services/concrete-slab',
+      fr:'services/dalle-de-beton',
     },
     {
       image :'excavation.jpg',
       name:'Mini excavation',
+      en:'services/excavation-services',
+      fr:'services/mini-excavation',
     },
     {
       image :'reparation-de-fissure.jpg',
       name:'repair',
+      en:'services/concrete-repair',
+      fr:'services/fissure-beton',
     },
 
   ];
